40fa3de2479893a54a32124b82d1d8615e1037f8
rlackdgml97 <rlackdgml97@naver.com>
2024-01-23 22:17:07 +0900

    0123 changhee Feat:이메일인증Controller작성중

--- demo/src/main/java/com/ssfay/racing/auth/controller/AuthController.java
+++
@@ -1,4 +0,0 @@
-package com.ssfay.racing.auth.controller;
-
-public class AuthController {
-}
--- demo/src/main/java/com/ssfay/racing/users/domain/UserJoinRequest.java
+++
@@ -1,7 +0,0 @@
-package com.ssfay.racing.users.domain;
-
-public class UserJoinRequest {
-    private String email;
-    private String password;
-    private String nickname;
-}
