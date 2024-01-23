9103f0dc0863476db36666863b1e4db8ce479842
rlackdgml97 <rlackdgml97@naver.com>
2024-01-23 22:17:07 +0900

    0123 changhee Feat:이메일인증Controller작성중

---
+++ demo/src/main/java/com/ssafy/racing/auth/domain/EmailPostDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.racing.auth.domain;
+
+import lombok.Getter;
+
+@Getter
+public class EmailPostDto {
+    private String email;
+}
---
+++ demo/src/main/java/com/ssafy/racing/game/domain/MyRacer.java
@@ -0,0 +1,23 @@
+package com.ssafy.racing.game.domain;
+
+import com.ssfay.racing.game.domain.Racer;
+import com.ssfay.racing.users.domain.Users;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "MyRacer")
+public class MyRacer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "myracer_id")
+    private int myracerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id")
+    private Users users;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "racer_id")
+    private Racer racer;
+}
