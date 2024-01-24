2cf3a0981325f404428b1f3aeb46f051743eef46
jww5555 <jww5555@naver.com>
2024-01-24 12:11:26 +0900

    jaewon

--- demo/src/main/java/com/ssafy/racing/users/domain/UserJoinRequest.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.racing.users.domain;
-
-public class UserJoinRequest {
-    private String email;
-    private String password;
-    private String nickname;
-}
--- demo/src/main/java/com/ssafy/racing/users/sevice/UserService.java
+++ demo/src/main/java/com/ssafy/racing/users/sevice/UserService.java
@@ -1,8 +1,11 @@
 package com.ssafy.racing.users.sevice;
+import com.ssafy.racing.users.domain.Users;
 import com.ssafy.racing.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +13,24 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public int changPassword(String usersEmail, String password){
+    // 비밀번호 변경
+    public int changePassword(String usersEmail, String password){
         return userRepository.updatePassword(usersEmail,password);
+    }
+
+    // 회원가입
+    // 아이디 중복 체크 (중복일 경우 true, 중복이 아닐 경우 false)
+    public boolean checkUsersEmailDuplicate(String usersEmail){
+        List<Users> usersList = userRepository.findByEmail(usersEmail);
+        boolean result = !usersList.isEmpty() ? true : false;
+        return result;
+    }
+
+    // 닉네임 중복체크 (중복일 경우 true, 중복이 아닐 경우 false)
+    public boolean checknicknameDuplicate(String nickname){
+        List<Users> usersList = userRepository.findByNickname(nickname);
+        boolean result = !usersList.isEmpty() ? true : false;
+        return result;
     }
 
 }
