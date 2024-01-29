fb4d577abe0f1fa8aaa7e9fd7dd4aab1ea8b60f1
최용훈 <cyh2607@gmail.com>
2024-01-29 17:16:35 +0900

    240129_FEAT.홈페이지_용훈

--- front/runzoo/src/pages/home/Home.js
+++ front/runzoo/src/pages/home/Home.js
@@ -1,17 +1,31 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Login from '../../components/home/Login';
+import SignUp from '../../components/home/Signup';
+import './Home.css';
+import GoogleLoginButton from '../../components/home/GoogleLoginButton';
 
 function Home() {
+  const navigate = useNavigate();
+
+  // 회원가입 버튼을 누르면 '/signup' 경로로 이동
+  const handleSignUpClick = () => {
+    navigate('/SignUp');
+  };
+
   return (
-    <div>
-      <h1>시작 페이지</h1>
-      <h2>컴포넌트</h2>
-      <p>뒷배경, 스위치, 입력칸</p>
-      <ul>
-        <h2>연결 페이지</h2>
-        <li>
-          <Link to="/Main">메인으로</Link>
-        </li>
-      </ul>
+    <div className='home-container'>
+      <div className='left-section'>
+        <h1>프로젝트 소개</h1>
+      </div>
+      <div className='right-section'>
+        <div>
+          <Login />
+          {/* 회원가입 버튼 추가 */}
+          <button onClick={handleSignUpClick}> 회원가입 </button>
+          <GoogleLoginButton />
+        </div>
+      </div>
     </div>
   );
 }
