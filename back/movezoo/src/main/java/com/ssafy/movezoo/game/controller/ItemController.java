14cd4dce768e392bfb95d3b0b43d90d37f2322f1
rlackdgml97 <rlackdgml97@naver.com>
2024-01-29 15:35:28 +0900

    240129 changhee Feat:merge
    Former-commit-id: 8487cc46e7518d7c14706461c9004b95ad15fd10

---
+++ back/movezoo/src/main/java/com/ssafy/movezoo/game/controller/ItemController.java
@@ -0,0 +1,17 @@
+package com.ssafy.movezoo.game.controller;
+
+import com.ssafy.movezoo.game.domain.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/item")
+public class ItemController {
+
+}
