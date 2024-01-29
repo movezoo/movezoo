3681e978d5bc9128d265946e735ee4bc53f1e999
wjdtpwls32 <wjdtpwls32@naver.com>
2024-01-29 11:45:58 +0900

    240129_feat.react start_세진
    Former-commit-id: f7063d88d583c7750154051ed4970de326d4cdee

---
+++ front/runzoo/src/pages/game/Game.js
@@ -0,0 +1,6 @@
+import { Link } from "react-router-dom";
+
+function Game() {
+  return <h1>게임 페이지</h1>;
+}
+export default Game;
---
+++ front/runzoo/src/pages/main/Main.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Main() {
+  return (
+    <div>
+      <h1>로비 페이지</h1>
+      <ul>
+        <h2>연결 페이지</h2>
+        <li>
+          <Link to="/">메인</Link>
+        </li>
+        <li>
+          <Link to="/">싱글</Link>
+        </li>
+        <li>
+          <Link to="/">멀티</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Main;
