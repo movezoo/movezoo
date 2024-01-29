93c49b152387824d48a5fbd6d20626a58661514f
HYEONMIN LEE <hyunmin2667@gmail.com>
2024-01-30 01:43:08 +0900

    240130 hyeonmin Feat:add new tfjs model and apply
    Former-commit-id: a3799f96396505c044820e43526a9f10af514ef9

--- front/game/src/MyOvVideo.js
+++ front/game/src/MyOvVideo.js
@@ -1,75 +1,74 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
 
 import * as tf from "@tensorflow/tfjs";
 // import "@tensorflow/tfjs";
-import * as posenet from "@tensorflow-models/posenet";
+import '@mediapipe/face_detection';
+import '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+// Register WebGL backend.
+import * as faceDetection from '@tensorflow-models/face-detection';
+
 import { data } from "./data.js";
 
 const MyOpenViduVideoComponent = (props) => {
   const { streamManager } = props;
-  // useRef로 웹캠, 캔버스, 게임 참조 변수 생성
-  const webcamRef = useRef(null);
+  const videoRef = useRef(null);
   const motionRef = useRef(null);
+  const [detector, setDetector] = useState(null);
 
   useEffect(() => {
-    if (!!webcamRef.current) {
+    if (!!videoRef.current) {
       // Add video element to StreamManager during initial rendering
-      streamManager.addVideoElement(webcamRef.current);
+      streamManager.addVideoElement(videoRef.current);
     }
 
-    // PosNet을 실행하는 함수
-    const runPosenet = async () => {
-      // PosNet 모델 로드
-      const net = await posenet.load({
-        inputResolution: { width: 640, height: 480 },
-        scale: 0.5,
-      });
-
-      // 일정 간격으로 detect 함수 실행
-
-      detect(net);
+    const initializeFaceDetector = async () => {
+      const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
+      const detectorConfig = {
+        runtime: 'tfjs',
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_detection'
+        // or 'base/node_modules/@mediapipe/face_detection' in npm.
+        // solutionPath:'./node_modules/@mediapipe/face_detection',
+      };
+      const faceDetector = await faceDetection.createDetector(model, detectorConfig);
+      setDetector(faceDetector);
     };
+    initializeFaceDetector();
 
-    // Pose를 감지하고 콘솔에 결과 출력하는 함수
-    const detect = async (net) => {
-      // console.log(tf.memory());
-      // 웹캠이 정상적으로 로드되었는지 확인
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null
-        // && webcamRef.current.video.readyState === 4
-      ) {
-        // 웹캠 비디오 및 크기 정보 가져오기
-        const video = webcamRef.current;
-        // const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.videoWidth;
-        const videoHeight = webcamRef.current.videoHeight;
-        // const videoWidth = webcamRef.current.video.videoWidth;
-        // const videoHeight = webcamRef.current.video.videoHeight;
+    const detectFaces = async () => {
+      if (detector && videoRef.current) {
+        const estimationConfig = { flipHorizontal: false };
 
-        /// 비디오 크기 설정
-        webcamRef.current.width = videoWidth;
-        webcamRef.current.height = videoHeight;
-        // webcamRef.current.video.width = videoWidth;
-        // webcamRef.current.video.height = videoHeight;
+        try {
+          const video = videoRef.current;
+          const videoWidth = video.videoWidth;
+          const videoHeight = video.videoHeight;
+          video.width = videoWidth;
+          video.height = videoHeight;
 
-        // PoseNet을 사용하여 포즈 추정
-        const pose = await net.estimateSinglePose(video);
+          const faces = await detector.estimateFaces(video, estimationConfig);
+          // console.log("Detected faces:", faces);
+          // 웹캠 비디오 및 크기 정보 가져오기
+
+
+          // console.log(faces);
+          // PoseNet을 사용하여 포즈 추정
         // console.log(pose)
 
         // 화면 기준 - 화면의 중앙을 기준으로 코의 좌표의 위치에 따른 진행 방향 결정, 민감도 설정 가능
         /**/
         const centerX = videoWidth / 2;
         let sensitivity = 50;
-        const poseNose = pose.keypoints[0].position.x;
+        
+        const noseX = faces[0]?.keypoints[2]?.x;
 
         // 결과에 따라 콘솔에 출력
-        if (poseNose < centerX - sensitivity) {
+        if (noseX < centerX - sensitivity) {
           data.isLeftKeyPressed = false;
           data.isRightKeyPressed = true;
           // console.log("Right");
-        } else if (poseNose > centerX + sensitivity) {
+        } else if (noseX > centerX + sensitivity) {
           data.isRightKeyPressed = false;
           data.isLeftKeyPressed = true;
           // console.log("Left");
@@ -78,78 +77,21 @@ const MyOpenViduVideoComponent = (props) => {
           data.isLeftKeyPressed = false;
         }
 
-        // 캔버스에 Pose 그리기
-        tf.tidy(() => {
-          drawCanvas(
-            pose,
-            video,
-            videoWidth,
-            videoHeight,
-            motionRef,
-            sensitivity
-          );
-          requestAnimationFrame(() => {
-            detect(net);
-          });
-        });
-      }
-    };
-
-    // 캔버스에 Pose를 그리는 함수
-    const drawCanvas = (
-      pose,
-      video,
-      videoWidth,
-      videoHeight,
-      canvas,
-      sensitivity = 0
-    ) => {
-      const ctx = canvas.current.getContext("2d");
-      canvas.current.width = videoWidth;
-      canvas.current.height = videoHeight;
-
-      // 가운데 수직 선 및 기준 수직 선 그리기
-      const centerX = videoWidth / 2;
-      drawLine(ctx, centerX, videoHeight, "blue");
-      if (sensitivity > 0) {
-        drawLine(ctx, centerX + sensitivity, videoHeight, "pink");
-        drawLine(ctx, centerX - sensitivity, videoHeight, "pink");
-      }
-
-      // Pose의 키포인트와 스켈레톤 그리기
-      drawKeypointsFlipped(pose["keypoints"], 0.5, ctx, centerX);
-    };
-
-    // 좌우 반전된 키포인트 그리기
-    const drawKeypointsFlipped = (keypoints, minConfidence, ctx, flipX) => {
-      for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
-
-        // 좌우 반전을 적용하여 x 좌표 계산
-        const x = flipX - (keypoint.position.x - flipX);
-
-        if (keypoint.score >= minConfidence) {
-          ctx.beginPath();
-          ctx.arc(x, keypoint.position.y, 3, 0, 2 * Math.PI);
-          ctx.fillStyle = "red";
-          ctx.fill();
+        } catch (error) {
+          console.error('Error detecting faces:', error);
         }
       }
     };
 
-    const drawLine = (ctx, X, Y, color) => {
-      ctx.beginPath(); // 초기화
-      ctx.moveTo(X, 0); // 시작점
-      ctx.lineTo(X, Y); // 도착점
-      ctx.lineWidth = 2; // 선 두께
-      ctx.strokeStyle = color;
-      ctx.stroke(); // 선 긋기
+    const runFaceDetection = async () => {
+      // Run face detection on each frame
+      detectFaces();
+      // Request animation frame for continuous detection
+      requestAnimationFrame(runFaceDetection);
     };
+    runFaceDetection();
 
-    // Posenet 실행
-
-    runPosenet();
-  }, [streamManager]);
+  }, [streamManager, detector]);
 
   return (
     <div className="PoseNet">
@@ -157,7 +99,7 @@ const MyOpenViduVideoComponent = (props) => {
         {/* 웹캠 및 캔버스 요소 추가 */}
         <video
           autoPlay={true}
-          ref={webcamRef}
+          ref={videoRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -172,7 +114,7 @@ const MyOpenViduVideoComponent = (props) => {
           }}
         />
 
-        <canvas
+        {/* <canvas
           ref={motionRef}
           style={{
             position: "absolute",
@@ -185,7 +127,7 @@ const MyOpenViduVideoComponent = (props) => {
             width: 640,
             height: 480,
           }}
-        />
+        /> */}
       </header>
     </div>
   );
