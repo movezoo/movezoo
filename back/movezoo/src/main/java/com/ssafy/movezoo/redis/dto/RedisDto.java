934b9b135e543d5090b46b73c640ce27e46c51c9
rlackdgml97 <rlackdgml97@naver.com>
2024-02-01 22:52:02 +0900

    240201 changhee Feat:develop redis

---
+++ back/movezoo/src/main/java/com/ssafy/movezoo/redis/dto/RedisDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.movezoo.redis.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RedisDto {
+    private String key;
+    private String val;
+}
