e361ec89196a5931e142e7ad7661af7b59e35807
leehyeonmin <hyunmin2667@gmail.com>
2024-02-02 10:13:25 +0900

    240202 hyeonmin Feat: insert game

--- front/movezoo/src/pages/multi/Multi.js
+++ front/movezoo/src/pages/multi/Multi.js
@@ -1,19 +1,9 @@
 import * as React from "react"
-import * as Popover from '@radix-ui/react-popover';
 import './Multi.css'
 
 function Multi() {
   return (
     <div>
-      <Popover.Root>
-    <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
-    <Popover.Portal>
-      <Popover.Content className="PopoverContent" sideOffset={5}>
-        Some more info…
-        <Popover.Arrow className="PopoverArrow" />
-      </Popover.Content>
-    </Popover.Portal>
-  </Popover.Root>
     </div>
   )
 }
