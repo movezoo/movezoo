89e2326daa416e48a60414c7a25557d75b9db5dd
rlackdgml97 <rlackdgml97@naver.com>
2024-02-02 16:30:13 +0900

    240201 changhee Feat:modi openvidu controller
    Former-commit-id: 9bdf6ccc814d5b6d640df0b008a1656bb0282aa0

--- back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/oauth/UserDetailsServiceImpl.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/oauth/UserDetailsServiceImpl.java
@@ -2,11 +2,14 @@ package com.ssafy.movezoo.auth.config.oauth;
 
 import com.ssafy.movezoo.user.domain.User;
 import com.ssafy.movezoo.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +25,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         // 시큐리티 세션에 세션 정보를 저장해 주는데 > session은 Authentication 타입 객체여야 한다.
         // 그리고 Authentication 안의 유저는 UserDetails 타입 객체여야 한다
         // => 시큐리티 세션(내부 Authentication(내부 UserDetails))
+//        User user = userRepository.findByEmail(userEmail)
+//                .orElseThrow(() -> new UsernameNotFoundException("not found useremail : " + userEmail));
         User user = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("not found useremail : " + userEmail));
+                .orElseThrow(() -> {
+                    // 사용자를 찾지 못한 경우 404 응답 코드를 클라이언트에게 반환
+                    HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    return new UsernameNotFoundException("not found useremail : " + userEmail);
+                });
+
 
         System.out.println("Logined User is : "+ user.toString());
 
