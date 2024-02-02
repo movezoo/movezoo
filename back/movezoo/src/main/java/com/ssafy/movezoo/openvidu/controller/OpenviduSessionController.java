9f784942ec0d2a8da0eae0ec1c03d5c570847723
rlackdgml97 <rlackdgml97@naver.com>
2024-02-02 12:02:50 +0900

    240201 changhee Feat:modi openvidu controller
    Former-commit-id: abb5f26b76112d2b3a8c08d06dc7cdc4cdc1f87c

--- back/movezoo/src/main/java/com/ssafy/movezoo/openvidu/controller/OpenviduSessionController.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/openvidu/controller/OpenviduSessionController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 public class OpenviduSessionController {
 
