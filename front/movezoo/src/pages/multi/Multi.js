bb075cecf5e86bd69903ae006f0fe2381157969d
wjdtpwls32 <wjdtpwls32@naver.com>
2024-02-04 03:45:44 +0900

    240204_feat.Multi page_sejin

--- front/movezoo/src/pages/multi/Multi.js
+++ front/movezoo/src/pages/multi/Multi.js
@@ -1,12 +1,50 @@
-import * as React from "react"
-import './Multi.css'
-
+import { useEffect, useState } from "react";
+import styles from "./Multi.module.css";
+import Back from "../../components/multi/Back";
+import Map from "../../components/multi/Map";
+import Chat from "../../components/multi/Chat";
+import Ready from "../../components/multi/Ready";
 
 function Multi() {
   return (
     <div>
+      <div className={styles.container}>
+        {/*왼쪽 영역*/}
+        <div className={styles.leftSection}>
+          <div className={styles.infoSection}>
+            <div className={styles.goBack}>
+              <Back />
+            </div>
+            <h1 style={{ margin: 10 }}>Multi Play</h1>
+          </div>
+          {/* {loading ? (
+            <h1 className={styles.txtLoading}>Loading...</h1>
+          ) : (
+            <Webcam className={styles.webCam} mirrored={true} />
+          )} */}
+
+          {/* <div>
+            <button className={styles.btnSelect}>카트 고르기</button>
+          </div> */}
+        </div>
+        {/*오른쪽 영역*/}
+        <div className={styles.rightSection}>
+          {/*맵*/}
+          <div className={styles.btnSelectMap}>
+            <Map />
+          </div>
+          {/* 채팅 */}
+          <div className={styles.areaChat}>
+            <Chat />
+          </div>
+          {/* 준비 버튼*/}
+          <div className={styles.btnReady}>
+            <Ready />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Multi
+export default Multi;
