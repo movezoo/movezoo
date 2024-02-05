fe2d893e864ded411229990b8eed3c98792267b5
wjdtpwls32 <wjdtpwls32@naver.com>
2024-02-05 11:56:54 +0900

    240205_feat.multi(game,result)_sejin

--- front/movezoo/src/pages/single/game/Game.js
+++ front/movezoo/src/pages/single/game/Game.js
@@ -1,13 +1,23 @@
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Game.module.css";
 import "./Game.module.css";
-// import Webcam from "react-webcam";
-import Back from "../../components/game/Back";
-
-import Main from "../../components/play/Main";
-import Cam from "../../components/play/Cam";
+import Webcam from "react-webcam";
+import Back from "../../../components/single/game/Back";
+import Main from "../../../components/play/Main";
+// import Cam from "../../../components/play/Cam";
 
 function Game() {
+  const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const { width, height } = containerRef.current.getBoundingClientRect();
+      setContainerSize({ width, height });
+    }
+  }, [containerRef]);
+
   return (
     <div>
       {/*일단 축소 화면*/}
@@ -17,7 +27,9 @@ function Game() {
           {/* <p style={{ textAlign: "center" }}>
             <strong>게임 화면</strong>
           </p> */}
-          <Main width={1150} height={700}/>
+          <div className={styles.game}>
+            <Main width={1100} height={660} />
+          </div>
 
           {/* 뒤로가면 메인 화면*/}
           <div className={styles.goBack}>
@@ -25,11 +37,8 @@ function Game() {
           </div>
 
           {/*웹캠*/}
-          {/* <div className={styles.webCam}> */}
-            <Cam />
-          {/* </div> */}
-
-          {/* <Webcam className={styles.webCam} mirrored={true} /> */}
+          {/* <Cam /> */}
+          <Webcam className={styles.webCam} mirrored={true} />
         </div>
 
         {/*오른쪽 화면*/}
