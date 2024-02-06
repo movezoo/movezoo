db36b6a6c2d7fd73c1d77fcfd8b0502577a29dcd
lement3864 <lement3864@gmail.com>
2024-02-06 11:25:44 +0900

    test

--- front/movezoo/src/pages/single/Single.js
+++ front/movezoo/src/pages/single/Single.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Single.module.css";
 import Webcam from "react-webcam";
 import Map2 from "../../components/single/Map2";
 import Back from "../../components/single/Back";
 import Start from "../../components/single/Start";
+import "./Single.css";
 
 function Single() {
   const [loading, setLoading] = useState(true);
@@ -15,35 +15,43 @@ function Single() {
   }, []);
   return (
     <div>
-      <div className={styles.container}>
-        {/*왼쪽 영역*/}
-        <div className={styles.leftSection}>
-          <div className={styles.infoSection}>
-            <div className={styles.goBack}>
-              <Back />
-            </div>
-            <h1 style={{ margin: 10 }}>Single Play</h1>
-          </div>
-          {loading ? (
-            <h1 className={styles.txtLoading}>Loading...</h1>
-          ) : (
-            <Webcam className={styles.webCam} mirrored={true} />
-          )}
-
-          <div>
-            <button className={styles.btnSelect}>카트 고르기</button>
+      <div className="single-container">
+        
+        {/* header */}
+        <div>
+          <h1 style={{ margin: 10 }}>Single Play</h1>
+          <div className="goBack">
+            <Back />
           </div>
         </div>
-        {/*오른쪽 영역*/}
-        <div className={styles.rightSection}>
-          {/*맵 & 최고기록*/}
-          <div className={styles.btnSelectMap}>
-            <Map2 />
+
+        {/*body*/}
+        <div>
+
+          <div className={styles.leftSection}>
+            <div className={styles.infoSection}>
+            </div>
+            {loading ? (
+              <h1 className={styles.txtLoading}>Loading...</h1>
+            ) : (
+              <Webcam className={styles.webCam} mirrored={true} />
+            )}
           </div>
-          {/* 시작 버튼*/}
-          <div className={styles.btnStart}>
-            <Start />
+          
+          <div className={styles.rightSection}>
+            {/*맵 & 최고기록*/}
+            <div className={styles.btnSelectMap}>
+              <Map2 />
+            </div>
+            <div className="charact-select">
+              <button className={styles.btnSelect}>카트 고르기</button>
+            </div>
+            {/* 시작 버튼*/}
+            <div className={styles.btnStart}>
+              <Start />
+            </div>
           </div>
+
         </div>
       </div>
     </div>
