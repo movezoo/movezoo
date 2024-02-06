7f3dc19692a6bd4cf4c429003ef44b36aa5d9e21
위재원 <jww5555@naver.com>
2024-02-06 12:12:03 +0900

    240206 jaewon Fix:Fixed currentUser

--- back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/SecurityConfig.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/SecurityConfig.java
@@ -80,6 +80,8 @@
                                             UserDetails userDetails2 = (UserDetails)session.getAttribute("user");
                                             log.info("session get ", userDetails2.getUsername());
 
+                                            
+
                                             // 성공 응답을 생성하거나 추가 작업 수행
                                             response.setStatus(HttpServletResponse.SC_OK);
                                         }
