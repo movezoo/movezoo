d4ef41c5c798a51b29065a7b750e7788d335dae9
위재원 <jww5555@naver.com>
2024-02-06 09:56:08 +0900

    240206 jaewon Fix:Fix Build Error
    Former-commit-id: 357e776a241136faa40f04258ff8ec15a02642cd

--- back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/details/CustomUserDetails.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/auth/config/details/CustomUserDetails.java
@@ -1,14 +1,15 @@
 package com.ssafy.movezoo.auth.config.details;
 
 import com.ssafy.movezoo.user.domain.User;
-import jakarta.annotation.Nullable;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 @Getter
 public class CustomUserDetails implements UserDetails, OAuth2User {
