bebf813cbfac1e05c107d6b1de7e41e8b04091d4
rlackdgml97 <rlackdgml97@naver.com>
2024-02-08 09:18:11 +0900

    240207 changhee Feat:develop coin controller
    Former-commit-id: 99abdced9c55bc7c1eb24aa7d37bbd710b8b8d21

--- back/movezoo/src/main/java/com/ssafy/movezoo/game/controller/CoinController.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/game/controller/CoinController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -32,6 +33,34 @@ public class CoinController {
 
         return ResponseEntity.status(HttpStatus.OK).body(coinResponseDto);
     }
+
+    @PostMapping
+    public ResponseEntity<SimpleResponseDto> gameReward(Authentication authentication, @RequestBody RoomSessionIdDto dto){
+        SimpleResponseDto simpleResponseDto = new SimpleResponseDto();
+        String roomSessionId = dto.getRoomSessionId();    // roomId: Long
+        String nickname = dto.getNickname();
+        int ranking= 0; //dto.getNickname;
+
+        //별별별 다른 페이지로 이동하는거 막아 화이트에이블뜨게, 아무것도 안뜨는거 에바"
+        //1. 닉네임으로 사용자를 입력받는다
+        //2. 사용자를 검증한다
+        //3. 맞다면 순위별로 재화지급
+
+        Optional<User> findUser = userService.findByNickname(nickname);
+        if(findUser.isEmpty() || authentication.getName() != user.getUserId()) {
+            simpleResponseDto.setSuccess(false);
+            simpleResponseDto.setMsg("사용자를 찯을 수 없습니다.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(simpleResponseDto);
+        }
+
+        User user = findUser.get();
+
+        userService.addUserCoin(user.getUserId(),reward[ranking]);
+
+        simpleResponseDto.setSuccess(true);
+        simpleResponseDto.setMsg("순위별 재화 지급 완료");
+        return ResponseEntity.status(HttpStatus.OK).body(simpleResponseDto);
+    }
     
 //    @PostMapping
 //    public ResponseEntity<SimpleResponseDto> gameReward(@RequestBody RoomSessionIdDto dto){
@@ -51,22 +80,22 @@ public class CoinController {
 //    }
 
 
-    @PostMapping
-    public ResponseEntity<SimpleResponseDto> gameReward(@RequestBody RoomSessionIdDto dto){
-        String roomSessionId = dto.getRoomSessionId();    // roomId: Long
-
-        List<SessionUserInfo> sessionUserList = findSessionUserList(roomSessionId);
-
-        for(int i = 0 ;i<sessionUserList.size(); i++){
-            SessionUserInfo sessionUserInfo = sessionUserList.get(i);
-            userService.addUserCoin(sessionUserInfo.getUserId(),reward[i]);
-        }
-
-        SimpleResponseDto simpleResponseDto = new SimpleResponseDto();
-        simpleResponseDto.setSuccess(true);
-        simpleResponseDto.setMsg("순위별 재화 지급 완료");
-        return ResponseEntity.status(HttpStatus.OK).body(simpleResponseDto);
-    }
+//    @PostMapping
+//    public ResponseEntity<SimpleResponseDto> gameReward(@RequestBody RoomSessionIdDto dto){
+//        String roomSessionId = dto.getRoomSessionId();    // roomId: Long
+//
+//        List<SessionUserInfo> sessionUserList = findSessionUserList(roomSessionId);
+//
+//        for(int i = 0 ;i<sessionUserList.size(); i++){
+//            SessionUserInfo sessionUserInfo = sessionUserList.get(i);
+//            userService.addUserCoin(sessionUserInfo.getUserId(),reward[i]);
+//        }
+//
+//        SimpleResponseDto simpleResponseDto = new SimpleResponseDto();
+//        simpleResponseDto.setSuccess(true);
+//        simpleResponseDto.setMsg("순위별 재화 지급 완료");
+//        return ResponseEntity.status(HttpStatus.OK).body(simpleResponseDto);
+//    }
 
 
     private List<SessionUserInfo> findSessionUserList(String roomSessionId) {
