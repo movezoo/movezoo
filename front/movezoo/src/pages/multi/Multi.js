abfebf66a1f36e662b3d1054a9f1bfc5c964e17b
leehyeonmin <hyunmin2667@gmail.com>
2024-02-08 14:08:38 +0900

    240208 hyeonmmin Fix: fix
    Former-commit-id: c2e740e4b203a432867ab94b47d4bba1a172b7be

--- front/movezoo/src/pages/multi/Multi.js
+++ front/movezoo/src/pages/multi/Multi.js
@@ -13,7 +13,7 @@ function Multi() {
     process.env.NODE_ENV === "production" ? "" : "https://i10e204.p.ssafy.io/";
 
   // 게임시작관리(props로 념겨줌)
-  const [isGameStart, setIsGameStart] = useState(false);
+  const [isPlayingGame, setIsPlayingGame] = useState(false);
   const [mySessionId, setMySessionId] = useState(null);
   const [myUserName, setMyUserName] = useState(
     "Participant" + Math.floor(Math.random() * 100)
@@ -112,6 +112,7 @@ function Multi() {
           });
           if (!existMyData) playerGameDataList.push(myGameData);
 
+          setIsPlayingGame(true)
           console.log(
             `joinsession : playerId init!!!!!!!! <${myGameData.playerId}>`
           );
@@ -132,7 +133,7 @@ function Multi() {
     if (session) {
       session.disconnect();
     }
-
+    setIsPlayingGame(false)
     setSession(undefined);
     setSubscribers([]);
     setMySessionId(null);
@@ -248,6 +249,7 @@ function Multi() {
           subscribers={subscribers}
           publisher={publisher}
           mySessionId={mySessionId}
+          isPlayingGame={isPlayingGame}
         />
       ) : null}
       {page === 4 ? (
