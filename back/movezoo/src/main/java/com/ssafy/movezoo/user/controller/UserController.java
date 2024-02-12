848ad2702dcd69e70754f0408253677ef4c4c2b6
jaewonwi <jww5555@naver.com>
2024-02-12 14:07:03 +0900

    240212 jaewon Fix:Social Login User Info, nickname Edit
    Former-commit-id: 231c2af307bd4fe5ab250ef4427a5e165c9644f2

--- back/movezoo/src/main/java/com/ssafy/movezoo/user/controller/UserController.java
+++ back/movezoo/src/main/java/com/ssafy/movezoo/user/controller/UserController.java
@@ -25,7 +25,6 @@ public class UserController {
     private final PasswordEncoder passwordEncoder;
     private final RacerService racerService;
 
-    private final String defaultProfileImgUrl = "./profileImg/profile1.png";
     // 회원가입
     @PostMapping
     public ResponseEntity<SimpleResponseDto> registUser(@RequestBody UserJoinRequestDto dto) {
@@ -39,7 +38,7 @@ public class UserController {
             msg = "중복된 닉네임입니다.";
         } else {
             // 회원가입 성공
-            userService.join(new User(dto.getUserEmail(), passwordEncoder.encode(dto.getPassword()), dto.getNickname(),defaultProfileImgUrl));
+            userService.join(new User(dto.getUserEmail(), passwordEncoder.encode(dto.getPassword()), dto.getNickname()));
 
             Optional<User> findUser = userService.findByEmail(dto.getUserEmail());
             findUser.ifPresent(user -> racerService.addMyRacer(user.getUserId(), 1));
@@ -69,50 +68,54 @@ public class UserController {
     // 닉네임 변경
     @PatchMapping("/nickname")
     public ResponseEntity<SimpleResponseDto> changeNickname(@RequestBody UserNicknameRequestDto dto, Authentication authentication) {
-        SimpleResponseDto simpleResponseDto = new SimpleResponseDto();
+        SimpleResponseDto simpleResponseDto = new SimpleResponseDto(false,"미인증");
 
-        simpleResponseDto.setMsg("미인증");
-        simpleResponseDto.setSuccess(false);
+//        if (authentication == null || dto.getUserEmail()==null || dto.getNickname()==null)
+        if (authentication == null || dto.getNickname() == null) {
+            simpleResponseDto.setMsg("authentication 객체 없음 / 닉네임 값이 없습니다.");
+            return ResponseEntity.badRequest().body(simpleResponseDto);
+        }
+//        User findUser = userService.findById(Integer.parseInt(authentication.getName()));
+//        if (findUser == null || !findUser.getUserEmail().equals(dto.getUserEmail()))
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(simpleResponseDto);
 
-        if (authentication == null || dto.getUserEmail()==null || dto.getNickname()==null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(simpleResponseDto);
-        
+        // authentication => 로그인한 사용자의 아이디(PK)를 가져옴
         User findUser = userService.findById(Integer.parseInt(authentication.getName()));
-        if (findUser == null || !findUser.getUserEmail().equals(dto.getUserEmail()))
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(simpleResponseDto);
+        if (findUser == null) {
+            simpleResponseDto.setMsg("사용자를 찾을 수 없습니다.");
+            return ResponseEntity.badRequest().body(simpleResponseDto);
+        }
 
         // 닉네임 중복체크
         if (userService.checkNicknameDuplicate(dto.getNickname())) {
             // 바꾸려는 닉네임이 DB에 있는 경우
-            Optional<User> optionalUser = userRepository.findByEmail(dto.getUserEmail());
-            if (optionalUser.isPresent()) {
-                if (optionalUser.get().getNickname().equals(dto.getNickname()))
-                    // 이미 내 닉네임인 경우
-                    simpleResponseDto.setMsg("현재 닉네임과 동일합니다.");
-                else {
-                    // 다른 사람이 가지고있는 닉네임인 경우
-                    simpleResponseDto.setMsg("중복된 닉네임 입니다.");
-                }
+            User user = userRepository.findById(Integer.parseInt(authentication.getName()));
 
-                simpleResponseDto.setSuccess(false);
-                return ResponseEntity.badRequest().body(simpleResponseDto);
-            }
+            if (user.getNickname().equals(dto.getNickname()))
+                // 이미 내 닉네임인 경우
+                simpleResponseDto.setMsg("현재 닉네임과 동일합니다.");
+            else
+                // 다른 사람이 가지고있는 닉네임인 경우
+                simpleResponseDto.setMsg("중복된 닉네임 입니다.");
+
+
+            return ResponseEntity.badRequest().body(simpleResponseDto);
         }
 
-        userService.changeNickname(dto.getUserEmail(), dto.getNickname());
+        userService.changeNickname(Integer.parseInt(authentication.getName()), dto.getNickname());
 
         simpleResponseDto.setSuccess(true);
         simpleResponseDto.setMsg("닉네임 변경 성공");
 
         return ResponseEntity.ok().body(simpleResponseDto);
-
     }
 
     // 프로필 이미지 변경
     @PatchMapping("/profile")
-    public ResponseEntity<SimpleResponseDto> changeProfile(@RequestBody UserProfileRequestDto dto) {
+    public ResponseEntity<SimpleResponseDto> changeProfile(@RequestBody UserProfileRequestDto dto, Authentication authentication) {
         SimpleResponseDto simpleResponseDto = new SimpleResponseDto(true, "프로필 변경 성공");
 
-        userService.changeProfileImg(dto.getUserEmail(), dto.getProfileImgUrl());
+        userService.changeProfileImg(Integer.parseInt(authentication.getName()), dto.getProfileImgUrl());
 
         return ResponseEntity.ok().body(simpleResponseDto);
     }
@@ -120,8 +123,8 @@ public class UserController {
 
     // 설정 변경
     @PatchMapping("/settings")
-    public ResponseEntity<SimpleResponseDto> changeSettings(@RequestBody UserSettingRequestDto dto) {
-        userService.changeSetting(dto.getUserId(), dto.getVolume(), dto.getMic(), dto.getCameraSensitivity());
+    public ResponseEntity<SimpleResponseDto> changeSettings(@RequestBody UserSettingRequestDto dto, Authentication authentication) {
+        userService.changeSetting(Integer.parseInt(authentication.getName()), dto.getVolume(), dto.getMic(), dto.getCameraSensitivity());
         SimpleResponseDto simpleResponseDto = new SimpleResponseDto(true, "설정 변경 완료");
 
         return ResponseEntity.ok().body(simpleResponseDto);
