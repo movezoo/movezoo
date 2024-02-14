469e87dcc095d49ad394362e2f4a06d9023d9456
lement3864 <lement3864@gmail.com>
2024-02-14 23:25:55 +0900

    240214 FEAT : volume mute add - hyunki
    Former-commit-id: 4563e0f46b6ffee7de1b651e4b1411d6d2c8ac82

--- front/movezoo/src/components/navbar/setting/Setting.js
+++ front/movezoo/src/components/navbar/setting/Setting.js
@@ -1,86 +1,29 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
-import { AiFillSetting, AiFillSound } from 'react-icons/ai';
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 import './Setting.css';
-import { IoCloseSharp } from "react-icons/io5";
-// import { AiFillSound } from "react-icons/ai";
 
 const Setting = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [volume, setVolume] = React.useState(20);
+  const [isMuted, setIsMuted] = React.useState(false);
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const handleVolumeChange = (event) => {
-    const newVolume = event.target.value;
-    setVolume(newVolume);
-  };
-
-  React.useEffect(() => {
+  const handleMute = () => {
+    setIsMuted(!isMuted);
     const audioElement = document.getElementById("background-audio");
-    audioElement.volume = volume / 100;
-  }, [volume]);
-
+    audioElement.volume = isMuted ? 1 : 0; // 음소거 상태에 따라 볼륨을 조절합니다.
+  };
 
   return (
     <div className='setting'>
-      <div className='setting-button-container' onClick={openModal}>
-        <AiFillSound className='settingButton'/>
-        {/* <AiFillSetting className='settingButton'/> */}
-      </div>
-
-      <Modal 
-      isOpen={isOpen} 
-      onRequestClose={closeModal}
-      style={{
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)', // 투명도를 0.75로 설정한 검은색 배경
-        },
-        content: {
-          width: '300px',
-          height: '170px',
-          margin: 'auto',
-          borderRadius: '30px',
-          backgroundColor: 'rgba(247, 254, 231, 0.6)', 
+      <div className='setting-button-container' onClick={handleMute}> {/* onClick 이벤트를 handleMute로 변경했습니다. */}
+        {
+          isMuted ? 
+            <FaVolumeMute className='settingButton' /> : // 음소거 아이콘이 보입니다.
+            <FaVolumeUp className='settingButton' /> // 소리 아이콘이 보입니다.
         }
-      }}
-      >
-        <div className='setting-container'>
-          <div className='setting-header'>
-            <div className='setting-header-name'>
-              
-            </div>
-            <div className='shop-header-exit'>
-              <IoCloseSharp className='exit-button' onClick={closeModal} />
-            </div>
-          </div>
-
-          <div className='setting-body'>
-            <div className='setting-sound'>
-              <div className='sound-image'>
-                <AiFillSound />
-              </div>
-              <div>
-                <input
-                    type="range"
-                    min="0"
-                    max="100"
-                    value={volume}
-                    onChange={handleVolumeChange}
-                  />
-              </div>
-            </div>
-          </div>
-        </div>
-        
-      </Modal>
+      </div>
     </div>
+
   );
 };
 
