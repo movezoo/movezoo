1f47f47caccb0b82edcf76f0f6f0a3fdc5c2f927
lement3864 <lement3864@gmail.com>
2024-02-15 16:46:05 +0900

    240215 TEST : api test coin - hyunki
    Former-commit-id: aa127a81447abffe358f41b3784cf85334b038c7

--- front/movezoo/src/pages/multi/result/Result.js
+++ front/movezoo/src/pages/multi/result/Result.js
@@ -97,8 +97,41 @@ function Result(props) {
         console.error('코인 업데이트 실패:', error);
       }
     }
+
+    const fetchUserCoin = async () => {
+      try {
+        const storedUserData = localStorage.getItem('userData');
+          if (!storedUserData) {
+              throw new Error('사용자 정보를 찾을 수 없습니다.');
+          }
+  
+        // 로컬 스토리지에서 조회한 데이터를 JSON 형태로 파싱
+        const userData = JSON.parse(storedUserData);
+  
+        // 코인 정보 조회 요청
+          const response = await axios.get(`https://i10e204.p.ssafy.io/api/user/${userData.userData.userId}`, {
+              withCredentials: true
+          });
+  
+          if (response.status === 200 && response.data) {
+              // Recoil 상태 및 로컬 스토리지 업데이트
+              const newCoinAmount = response.data.coin;
+              console.log(newCoinAmount)
+              setCoin(newCoinAmount); // Recoil 상태 업데이트
+             
+              let updatedUserData = { ...userData };
+              updatedUserData.userData.coin = newCoinAmount;
+              localStorage.setItem('userData', JSON.stringify(updatedUserData));
+          }
+  
+  
+      } catch (error) {
+        console.error('유저 코인 정보 요청 실패:', error);
+      }
+    }
   
     updateCoin();
+    fetchUserCoin();
   }, [userIds]);
 
   // 유저 코인 정보 업데이트
