8d756075f022b4c3619873dd7e3cb611131a1293
lement3864 <lement3864@gmail.com>
2024-02-19 09:21:40 +0900

    240219 record 0 block - hyunki
    Former-commit-id: 27cf2f2b9412244a2acdbcfd16d8ffa940ab675d

--- front/movezoo/src/components/single/result/Record.js
+++ front/movezoo/src/components/single/result/Record.js
@@ -1,3 +1,92 @@
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { singleResultState } from "../../../components/state/gameState.js"
+// import { useRecoilState } from "recoil";
+
+
+// import "./Record.css";
+
+
+
+// function Record() {
+//   const [userLaptime, setUserLaptime] = useState(null);
+//   const [ singleResult ] = useRecoilState(singleResultState);
+
+  
+//   useEffect(() => {
+//     const fetchUserLaptime = async () => {
+//       try {
+//         const storedUserData = localStorage.getItem('userData');
+//         if (!storedUserData) {
+//             throw new Error('사용자 정보를 찾을 수 없습니다.');
+//         }
+//         const userData = JSON.parse(storedUserData);    
+//         const userId = userData.userData.userId;
+//         const mapNumber = userData.selectedMapId;
+
+//         // console.log(userId)
+//         // console.log(mapNumber)
+        
+//         const { data: userLaptime } = await axios.get(`https://i10e204.p.ssafy.io/api/laptime/${userId}/${mapNumber}`);
+        
+        
+//         // 이번 게임 랩타임 db에 보내기
+//         if (userLaptime.record > singleResult.time ) {
+//           try {
+//             // console.log(userId)
+//             // console.log(mapNumber)
+//             // console.log(userLaptime.record)
+//             // console.log(singleResult.time)
+//             const updateLaptime = await axios.patch('https://i10e204.p.ssafy.io/api/laptime', 
+//             { userId, trackId: mapNumber, record: singleResult.time });
+            
+//             // console.log('랩타임 업데이트 성공:', updateLaptime);
+//           } catch (error) {
+//             console.error('랩타임 업데이트 실패:', error);
+//           }
+//         }
+        
+//         // db에 랩타임이 없으면 이번 게임 랩타임을 db에 보내기
+//         if (!userLaptime.record) {
+//           try {
+//             const updateLaptime = await axios.patch('https://i10e204.p.ssafy.io/api/laptime', 
+//             { userId, trackId: mapNumber, record: singleResult.time });
+    
+//             // console.log('랩타임 업데이트 성공:', updateLaptime);
+//           } catch (error) {
+//             console.error('랩타임 업데이트 실패:', error);
+//           }
+//         }
+        
+//         setUserLaptime(userLaptime);
+//         // console.log(userLaptime);
+  
+//       } catch (error) {
+//         console.error('랭킹 정보 요청 실패:', error);
+//       }
+//     }
+  
+//     fetchUserLaptime();
+    
+//   }, [singleResult.time]);
+
+
+
+//   return (
+//     <div className="Record-body">
+//       <div className="title">LAP TIME</div>
+//       <div className="time">{singleResult.time}</div>
+//       <div className="title">BEST</div>
+//       <div className="time">{userLaptime ? userLaptime.record : 'Loading...'}</div>
+//     </div>
+//   );
+// }
+
+// export default Record;
+
+
+// test
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { singleResultState } from "../../../components/state/gameState.js"
@@ -31,12 +120,8 @@ function Record() {
         
         
         // 이번 게임 랩타임 db에 보내기
-        if (userLaptime.record > singleResult.time ) {
+        if (userLaptime.record > singleResult.time && singleResult.time !== 0) {
           try {
-            // console.log(userId)
-            // console.log(mapNumber)
-            // console.log(userLaptime.record)
-            // console.log(singleResult.time)
             const updateLaptime = await axios.patch('https://i10e204.p.ssafy.io/api/laptime', 
             { userId, trackId: mapNumber, record: singleResult.time });
             
@@ -47,11 +132,11 @@ function Record() {
         }
         
         // db에 랩타임이 없으면 이번 게임 랩타임을 db에 보내기
-        if (!userLaptime.record) {
+        if (!userLaptime.record && singleResult.time !== 0) {
           try {
             const updateLaptime = await axios.patch('https://i10e204.p.ssafy.io/api/laptime', 
             { userId, trackId: mapNumber, record: singleResult.time });
-    
+        
             // console.log('랩타임 업데이트 성공:', updateLaptime);
           } catch (error) {
             console.error('랩타임 업데이트 실패:', error);
